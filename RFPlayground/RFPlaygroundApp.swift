4c15c2bc77fb43885ca7fb9c6094ee13f9f90fd5
setzeus <jnajera1917@gmail.com>
2023-01-12 20:11:01 -0500

    Initial Commit

---
+++ RFPlayground/RFPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RFPlaygroundApp.swift
+//  RFPlayground
+//
+//  Created by Jesus Najera Restfully on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct RFPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
