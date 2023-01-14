77ab73c6a7f2713f6b61da7b86162b8610818505
setzeus <jnajera1917@gmail.com>
2023-01-14 16:42:10 -0500

    storekit configuration & prdocut fetch

--- RFPlayground/ContentView.swift
+++ RFPlayground/ContentView.swift
@@ -6,17 +6,40 @@
 //
 
 import SwiftUI
+import StoreKit
 
 struct ContentView: View {
+    
+    let productIds = ["01","03"]
+
+    @State private var products: [Product] = []
+
+    //let products = try await Product.products(for: productIds)
+
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Products")
+            ForEach(self.products) { product in
+                Button(action: {
+                    // do nothing for now
+                }, label: {
+                    Text("\(product.displayName) - \(product.displayPrice)")
+                })
+            }
+        }.task {
+            do {
+                try await self.loadProducts()
+            } catch {
+                print(error)
+            }
         }
-        .padding()
     }
+    
+    private func loadProducts() async throws {
+        self.products = try await Product.products(for: productIds)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
