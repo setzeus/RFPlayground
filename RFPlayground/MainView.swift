34e12dba5c79389a710c62c8a8a8fb93488057be
setzeus <jnajera1917@gmail.com>
2023-01-29 11:09:51 -0500

    continued setup

--- RFPlayground/MainView.swift
+++ RFPlayground/MainView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct MainView: View {
     
     let staticTimes = ["6 a", "8 a", "10 a", "12 p", "2 p", "4 p", "6 p", "8 p", "10 p", "12 a", "2 a", "4 a"]
+    let staticWeekDay = ["Sun", "Sat", "Fri", "Thur", "Wed", "Tue", "Mon", "Sun", "Sat", "Fri"]
+    let staticWeekNum = ["29", "28", "27", "26", "25", "24", "23", "22", "21", "20"]
+    let daysDisplayed = [0,1,2,3,4,5,6,7,8,9]
     
     var body: some View {
         // View Container
@@ -21,7 +24,8 @@ struct MainView: View {
                     VStack {
                         Text("Jan").fontWeight(.bold)
                         Text("2023").fontWeight(.bold)
-                    }
+                    }.frame(height: 50)
+                        .background(Color.red)
                     VStack {
                         ForEach(staticTimes, id: \.self) {time in
                             if time == staticTimes[0] {
@@ -35,10 +39,27 @@ struct MainView: View {
                     }
                 }
                 ScrollView(.horizontal) {
-                    VStack {
-                        Text("Jan")
-                        Text("2023")
+                    
+                    ForEach(daysDisplayed, id: \.self) { i in
+                        
+                        HStack(spacing: 20) {
+                                ForEach(0..<10) {
+                                    Text("Item \($0)")
+                                        .foregroundColor(.white)
+                                        .font(.largeTitle)
+                                        .background(.red)
+                                }
+                            }
+                        //Text(staticWeekDay[i]).fontWeight(.bold)
+//                        VStack {
+//                            VStack {
+//                                Text(staticWeekDay[i]).fontWeight(.bold)
+//                                Text(staticWeekNum[i]).fontWeight(.bold)
+//                            }.frame(height: 75)
+//                                .background(Color.red)
+//                        }
                     }
+                    
                 }
             }.frame(maxHeight: .infinity)
                 .background(Color.blue)
